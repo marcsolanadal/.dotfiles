90724b4d1af66f22909e25aa5e6f3eac3e6b249c
Marc Solà Nadal <ionthasMail@gmail.com>
2015-02-09 17:17:31 +0100

    Added Mac OS X Darwin for the zelda battery script and modified the behaviour of the script

--- scripts/zelda_battery.sh
+++ scripts/zelda_battery.sh
@@ -1,42 +1,37 @@
 #!/bin/bash
 
 HEART='♥'
-TRIFORCE_PART='▲'
+TRIFORCE_PART='▲ '
 NUM_HEARTS=10
 
 # Zenburn colors
-#GOLD='#[fg=colour223]'
-#RED='#[fg=colour174]'
-#GRAY='#[fg=colour248]'
-
-# Solarize colors
 GOLD='#[fg=colour223]'
-RED='#red'
-GRAY='#gray'
-
+RED='#[fg=colour174]'
+GRAY='#[fg=colour248]'
 
 if [[ `uname` == 'Linux' ]]; then
   current_charge=$(cat /sys/class/power_supply/BAT0/energy_now)
   total_charge=$(cat /sys/class/power_supply/BAT0/energy_full)
   charging=$(cat /sys/class/power_supply/ADP1/online)
+elif [[ `uname` == 'Darwin' ]]; then
+  current_charge=$(ioreg -l | grep -i CurrentCapacity | awk '{print $5}')
+  total_charge=$(ioreg -l | grep -i MaxCapacity | awk '{print $5}')
+  connected=$(pmset -g ps | head -1 | grep "AC")
+  [[ -z $connected ]] && charging='1' || charging=''
 fi
 
 charged_slots=$(echo "(($current_charge/$total_charge)*$NUM_HEARTS)+1" |
-  bc -l | cut -d '.' -f 1)
+bc -l | cut -d '.' -f 1)
 
 if [[ $charged_slots -gt $NUM_HEARTS ]]; then
   charged_slots=$NUM_HEARTS
 fi
 
-echo -n $GOLD
-if [[ $charging == '' ]]; then
-  echo -n "$TRIFORCE_PART"
-  exit
-fi
 if [[ $charged_slots == $NUM_HEARTS ]]; then
+  echo -n $GOLD
   echo -n "$TRIFORCE_PART"
 else
-  if [[ $charging == 1 ]]; then
+  if [[ $charging == '' ]]; then
     echo -n $GOLD
   else
     echo -n $RED
