283426aefe7189b93cd6fb06e49e924eff16042c
Marc Solà Nadal <ionthasMail@gmail.com>
2015-02-12 13:31:16 +0100

    Unified the way we initialize bash within diferent systems

--- install.sh
+++ install.sh
@@ -3,9 +3,10 @@
 git config --global user.name "Marc Solà Nadal"
 git config --global user.email "ionthasMail@gmail.com"
 
-rm -rf ~/.emacs ~/.vim ~/.vimrc ~/.bashrc ~/.tmux.conf ~/.ssh
+rm -rf ~/.emacs ~/.vim ~/.vimrc ~/.bashrc ~/.tmux.conf ~/.bash_profile
 
 ln -s ~/.dotfiles/config/bashrc ~/.bashrc
 ln -s ~/.dotfiles/config/vimrc ~/.vimrc
 ln -s ~/.dotfiles/config/tmux.conf ~/.tmux.conf
 ln -s ~/.dotfiles/config/emacs ~/.emacs
+ln -s ~/.dotfiles/config/bash_profile ~/.bash_profile
