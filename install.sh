230eac40cebfef1f01909b903bf045d963b55f89
Marc Solà Nadal <ionthasMail@gmail.com>
2015-08-15 13:15:01 -0400

    Typo solved inside tmux.conf

--- install.sh
+++ install.sh
@@ -8,5 +8,5 @@ rm -rf ~/.emacs ~/.vim ~/.vimrc ~/.bashrc ~/.tmux.conf ~/.bash_profile
 ln -s ~/.dotfiles/config/bashrc ~/.bashrc
 ln -s ~/.dotfiles/config/vimrc ~/.vimrc
 ln -s ~/.dotfiles/config/tmux.conf ~/.tmux.conf
-ln -s ~/.dotfiles/config/emacs ~/.emacs
+#ln -s ~/.dotfiles/config/emacs ~/.emacs
 ln -s ~/.dotfiles/config/bash_profile ~/.bash_profile
