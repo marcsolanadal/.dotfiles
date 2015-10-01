736567907e9d7913bc1b27b7936e3988b80974ed
Marc Solà Nadal <ionthasMail@gmail.com>
2015-10-01 16:18:11 +0200

    NTP configuration on install and fixed tmux attach problems
    Why:
    
    We had the wrong timezone and the tmux attach didn't work correctly.
    We also had the irssi folder hidden in the config folder.
    
    This change addresses the need by:
    
    Added some script lines in install.sh to fix the problems with ntp
    and changed the line that attach to tmux session.

--- install.sh
+++ install.sh
@@ -1,12 +1,21 @@
 #!/bin/bash
 
-git config --global user.name "Marc Solà Nadal"
-git config --global user.email "ionthasMail@gmail.com"
+# Setting up NTP
+sudo apt-get install ntp ntpdate -y
 
-rm -rf ~/.emacs ~/.vim ~/.vimrc ~/.bashrc ~/.tmux.conf ~/.bash_profile
+# Local clock
+sudo ln -sf /usr/share/zoneinfo/Europe/Andorra /etc/localtime
+sudo hwclock --utc --systohc
+
+# Setting up GIT
+rm -rf ~/.gitconfig ~/.gitmessage
+ln -s ~/.dotfiles/config/gitconfig ~/.gitconfig
+ln -s ~/.dotfiles/config/gitmessage ~/.gitmessage
+
+rm -rf ~/.emacs ~/.vim ~/.vimrc ~/.bashrc ~/.tmux.conf ~/.bash_profile ~/.irssi
 
 ln -s ~/.dotfiles/config/bashrc ~/.bashrc
 ln -s ~/.dotfiles/config/vimrc ~/.vimrc
 ln -s ~/.dotfiles/config/tmux.conf ~/.tmux.conf
-#ln -s ~/.dotfiles/config/emacs ~/.emacs
 ln -s ~/.dotfiles/config/bash_profile ~/.bash_profile
+ln -s ~/.dotfiles/config/.irssi ~/.irssi
